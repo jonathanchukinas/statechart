085e4a77d87c7b3477154e9250b7b3bfec7de668
Jonathan Chukinas (system76) <chukinas@gmail.com>
2022-01-26 20:43:52 -0500

    initial commit

---
+++ lib/state_chart.ex
@@ -0,0 +1,18 @@
+defmodule StateChart do
+  @moduledoc """
+  Documentation for `StateChart`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> StateChart.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/state_chart_test.exs
@@ -0,0 +1,8 @@
+defmodule StateChartTest do
+  use ExUnit.Case
+  doctest StateChart
+
+  test "greets the world" do
+    assert StateChart.hello() == :world
+  end
+end
